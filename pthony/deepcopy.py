6271074f97b4a7d09fe7300936d9707e5ae83eea
aneela-gth <aneelabandi9951@gmail.com>
2025-10-26 21:07:25 +0530

    deepcopy&shallowcopy.py

--- pthony/deepcopy.py
+++ pthony/deepcopy.py
@@ -48,3 +48,13 @@ print(sharanya)
 import copy
 x=[[1,2,3],[4,5],6]
 shallow=copy.deepcopy(x)
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+terms = int(input("Enter the number of terms: "))
+
+print("Fibonacci Series:")
+for i in range(terms):
+    print(fibonacci(i), end=" ")
